5d39c2aefd2886630f10b97193c9fe33abc83d5f
lakindu2002 <lakindu2002@live.com>
2021-07-05 19:07:05 +0530

    changed colors

--- app/src/main/java/com/cb007787/timetabler/MainActivity.java
+++ app/src/main/java/com/cb007787/timetabler/MainActivity.java
@@ -1,12 +1,8 @@
 package com.cb007787.timetabler;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.cb007787.timetabler.view.shared.SharedContainer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,12 +10,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        new Handler().postDelayed(() -> {
-            //new runnable instance
-            Intent theIntent = new Intent(MainActivity.this, SharedContainer.class);
-            startActivity(theIntent);
-            finish();
-        }, 1000);
     }
 }
