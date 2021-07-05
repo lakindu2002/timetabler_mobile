f1de4609244a75b904d4e61ee3b5cf392604a924
lakindu2002 <lakindu2002@live.com>
2021-07-05 22:42:16 +0530

    changed background color, added pw reset and completed login UI

---
+++ app/src/main/java/com/cb007787/timetabler/common/ResetPasswordFragment.java
@@ -0,0 +1,28 @@
+package com.cb007787.timetabler.common;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.cb007787.timetabler.R;
+
+public class ResetPasswordFragment extends Fragment {
+
+    public ResetPasswordFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_reset_password, container, false);
+    }
+}
