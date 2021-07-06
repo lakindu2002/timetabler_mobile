1457d7638e6fe2b457bf8d73b49d6e369cbf2b0d
lakindu2002 <lakindu2002@live.com>
2021-07-06 13:17:58 +0530

    changed the color scheme on snackbar

--- app/src/main/java/com/cb007787/timetabler/common/ResetPasswordFragment.java
+++ app/src/main/java/com/cb007787/timetabler/common/ResetPasswordFragment.java
@@ -41,7 +41,7 @@ public class ResetPasswordFragment extends Fragment {
     private void constructInitialSnackbar() {
         //requireView - Get the root view for the fragment's layout (the one returned by onCreateView).
         Snackbar theSnackbar = Snackbar.make(requireView(), getString(R.string.reset_info_text), Snackbar.LENGTH_INDEFINITE);
-        theSnackbar.setBackgroundTint(getResources().getColor(R.color.btn_danger, null));
+        theSnackbar.setBackgroundTint(getResources().getColor(R.color.btn_primary, null));
         //add a dismiss button
         theSnackbar.setAction("Dismiss", new View.OnClickListener() {
             @Override
