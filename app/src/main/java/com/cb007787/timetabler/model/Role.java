b7f8dd8886bc108d87d023f7f4426b8598a166bb
Lakindu Hewawasam <lakindu2002@live.com>
2021-07-27 12:21:27 +0530

    added the role and user dto

---
+++ app/src/main/java/com/cb007787/timetabler/model/Role.java
@@ -0,0 +1,25 @@
+package com.cb007787.timetabler.model;
+
+public class Role {
+    private int roleId;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ app/src/main/java/com/cb007787/timetabler/model/User.java
@@ -0,0 +1,108 @@
+package com.cb007787.timetabler.model;
+
+import java.util.Date;
+
+public class User {
+    private String username;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private String contactNumber;
+    private Date dateOfBirth;
+    private Date memberSince;
+    private int age;
+    private String password;
+    private String reEnterPassword;
+    private Role userRole;
+
+    public User() {
+    }
+
+    public Role getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(Role userRole) {
+        this.userRole = userRole;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getMemberSince() {
+        return memberSince;
+    }
+
+    public void setMemberSince(Date memberSince) {
+        this.memberSince = memberSince;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getReEnterPassword() {
+        return reEnterPassword;
+    }
+
+    public void setReEnterPassword(String reEnterPassword) {
+        this.reEnterPassword = reEnterPassword;
+    }
+}
