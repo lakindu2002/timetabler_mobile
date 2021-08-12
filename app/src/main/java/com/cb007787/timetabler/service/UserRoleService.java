9c8f590045605f24ce2cb9c9298b29c0c5593c6f
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-12 19:38:32 +0530

    added role service

--- app/src/main/java/com/cb007787/timetabler/service/UserRoleService.java
+++ app/src/main/java/com/cb007787/timetabler/service/UserRoleService.java
@@ -1,4 +1,16 @@
 package com.cb007787.timetabler.service;
 
+import com.cb007787.timetabler.model.Role;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+
 public interface UserRoleService {
+    String BASE_ENDPOINT = "roles/";
+
+    @GET(BASE_ENDPOINT + "getAllExceptSystemAdmin")
+    Call<List<Role>> getAllRolesWithoutSystemAdmin(@Header("Authorization") String token);
 }
