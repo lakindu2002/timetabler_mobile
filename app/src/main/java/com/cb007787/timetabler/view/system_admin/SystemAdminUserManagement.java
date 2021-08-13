56ad839be79723946ca786323509d5d3e805ae17
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-13 14:30:00 +0530

    added bottom navigation for user management

--- app/src/main/java/com/cb007787/timetabler/view/system_admin/SystemAdminUserManagement.java
+++ app/src/main/java/com/cb007787/timetabler/view/system_admin/SystemAdminUserManagement.java
@@ -1,5 +1,6 @@
 package com.cb007787.timetabler.view.system_admin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
@@ -8,19 +9,21 @@ import androidx.fragment.app.FragmentContainerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.cb007787.timetabler.R;
 import com.cb007787.timetabler.service.PreferenceInformation;
 import com.cb007787.timetabler.service.SharedPreferenceService;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-public class SystemAdminUserManagement extends AppCompatActivity {
+public class SystemAdminUserManagement extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     private Toolbar theToolbar;
-    private FloatingActionButton createButton;
     private FragmentContainerView fragmentContainerView;
+    private BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,10 +41,7 @@ public class SystemAdminUserManagement extends AppCompatActivity {
             supportActionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_home_24);
         }
 
-        createButton.setOnClickListener(v -> {
-            //once user clicks floating action button, show the create user dialog fragment.
-            startActivity(new Intent(this, SystemAdminCreateUser.class));
-        });
+        navigationView.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -56,7 +56,16 @@ public class SystemAdminUserManagement extends AppCompatActivity {
 
     private void getReferences() {
         this.theToolbar = findViewById(R.id.the_toolbar);
-        this.createButton = findViewById(R.id.create_action_button);
         fragmentContainerView = findViewById(R.id.fragment_container_view);
+        navigationView = findViewById(R.id.bottom_navigation);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.new_user) {
+            startActivity(new Intent(this, SystemAdminCreateUser.class));
+            return true;
+        }
+        return false;
     }
 }
