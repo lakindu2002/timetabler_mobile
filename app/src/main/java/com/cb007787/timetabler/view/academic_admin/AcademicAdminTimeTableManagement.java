5ac5aa02f913e0eb58c3a6e9e969633bb2b32e48
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-18 13:18:34 +0530

    added timetable components

---
+++ app/src/main/java/com/cb007787/timetabler/view/academic_admin/AcademicAdminTimeTableManagement.java
@@ -0,0 +1,16 @@
+package com.cb007787.timetabler.view.academic_admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cb007787.timetabler.R;
+
+public class AcademicAdminTimeTableManagement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_academic_admin_time_table_management);
+    }
+}
