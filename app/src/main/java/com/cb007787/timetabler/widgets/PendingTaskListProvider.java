9ffcda946106c4a7f0233fdb3519b5c744711656
Lakindu Hewawasam <lakindu2002@live.com>
2021-08-24 23:19:06 +0530

    implemented app widget

---
+++ app/src/main/java/com/cb007787/timetabler/widgets/PendingTaskListProvider.java
@@ -0,0 +1,29 @@
+package com.cb007787.timetabler.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.cb007787.timetabler.R;
+
+public class PendingTaskListProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        //app widget ids - all instances of widgets on home screen as more than one widget can be available at a time
+        for (int widgetInstanceId : appWidgetIds) {
+            //iterate over each instance of the app widget inflated on the home screen.
+
+            //define the service required to load data for the StackView
+            Intent pendingTaskService = new Intent(context, PendingTaskListService.class);
+            //remote view because view is accessed in another process
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget_pending_task_list);
+            remoteViews.setRemoteAdapter(R.id.task_list, pendingTaskService); //set the adapter
+            remoteViews.setEmptyView(R.id.task_list, R.id.empty_list); //when no data, show message
+
+            appWidgetManager.updateAppWidget(widgetInstanceId, remoteViews); //update the UI on the widget.
+        }
+    }
+}
---
+++ app/src/main/java/com/cb007787/timetabler/widgets/PendingTaskListService.java
@@ -0,0 +1,157 @@
+package com.cb007787.timetabler.widgets;
+
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import com.cb007787.timetabler.R;
+import com.cb007787.timetabler.model.AuthReturn;
+import com.cb007787.timetabler.model.Task;
+import com.cb007787.timetabler.provider.TaskContentProvider;
+import com.cb007787.timetabler.provider.TaskDbHelper;
+import com.cb007787.timetabler.service.PreferenceInformation;
+import com.cb007787.timetabler.service.SharedPreferenceService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class PendingTaskListService extends RemoteViewsService {
+    //the service class that is used to fetch data for a dynamic list
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        //remote views factory - adapter in recycler view
+        return new PendingTaskListViewFactory(getApplicationContext(), new ArrayList<>());
+    }
+
+    public static class PendingTaskListViewFactory implements RemoteViewsFactory {
+        //define the adapter for the StackView.
+        private final Context context;
+        private final List<Task> taskList;
+        private AuthReturn authReturn;
+        private final SimpleDateFormat dateFormat;
+        private final SimpleDateFormat createFormat;
+        private Cursor retrievedQuery;
+
+        public PendingTaskListViewFactory(Context context, List<Task> taskList) {
+            this.context = context;
+            this.taskList = taskList;
+            dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH);
+            createFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss", Locale.ENGLISH);
+        }
+
+        @Override
+        public void onCreate() {
+            //connect to database and get data to load to the widget.
+            try {
+                authReturn = SharedPreferenceService.getLoggedInUser(context, PreferenceInformation.PREFERENCE_NAME);
+            } catch (JsonProcessingException e) {
+                System.out.println("ERROR ON PendingTaskListViewFactory");
+            }
+
+            if (authReturn != null) {
+                String[] columnsRequired = {
+                        TaskDbHelper.TASK_NAME,
+                        TaskDbHelper.START_DATE,
+                        TaskDbHelper.DUE_DATE,
+                        TaskDbHelper.TASK_CREATED_AT,
+                        TaskDbHelper.TASK_STATUS
+                };
+                String[] whereArgs = {"Pending", authReturn.getUsername()};
+                retrievedQuery = context.getContentResolver().query(TaskContentProvider.PERFORM_ALL_PENDING_URI, columnsRequired, null, whereArgs, "ASC");
+            }
+        }
+
+        @Override
+        public void onDataSetChanged() {
+            if (retrievedQuery != null && !retrievedQuery.isClosed()) {
+                if (retrievedQuery.moveToFirst()) {
+                    //row exists
+                    taskList.clear();
+                    while (!retrievedQuery.isAfterLast()) {
+                        //iterate over the rows
+                        String taskName = retrievedQuery.getString(retrievedQuery.getColumnIndex(TaskDbHelper.TASK_NAME));
+                        String taskStatus = retrievedQuery.getString(retrievedQuery.getColumnIndex(TaskDbHelper.TASK_STATUS));
+                        long startDate = retrievedQuery.getLong(retrievedQuery.getColumnIndex(TaskDbHelper.START_DATE));
+                        long dueDate = retrievedQuery.getLong(retrievedQuery.getColumnIndex(TaskDbHelper.DUE_DATE));
+                        long createDate = retrievedQuery.getLong(retrievedQuery.getColumnIndex(TaskDbHelper.TASK_CREATED_AT));
+
+                        Task eachTask = new Task();
+                        eachTask.setTaskName(taskName);
+                        eachTask.setStartDateInMs(startDate);
+                        eachTask.setDueDateInMs(dueDate);
+                        eachTask.setTaskCreatedAtInMs(createDate);
+                        eachTask.setTaskStatus(taskStatus);
+
+                        taskList.add(eachTask);
+
+                        retrievedQuery.moveToNext(); //go to next row.
+                    }
+                }
+                retrievedQuery.close(); //close query.
+            }
+        }
+
+
+        @Override
+        public void onDestroy() {
+            if (retrievedQuery != null) {
+                retrievedQuery.close();
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return taskList.size();
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            if (taskList.size() != 0) {
+                //if there are objects present.
+                Task taskAtPosition = taskList.get(position);
+                RemoteViews viewForEachCard = new RemoteViews(context.getPackageName(), R.layout.widget_pending_each_card);
+                viewForEachCard.setTextViewText(R.id.widget_task_name, taskAtPosition.getTaskName());
+                viewForEachCard.setTextViewText(R.id.widget_created, "Created: " + createFormat.format(new Date(taskAtPosition.getTaskCreatedAtInMs())));
+                viewForEachCard.setTextViewText(R.id.widget_duration, String.format(
+                        "From %s to %s", dateFormat.format(new Date(taskAtPosition.getStartDateInMs())),
+                        dateFormat.format(new Date(taskAtPosition.getDueDateInMs()))
+                ));
+                viewForEachCard.setTextViewText(R.id.widget_status, String.format("Status: %s", taskAtPosition.getTaskStatus()));
+
+                return viewForEachCard;
+            } else {
+                return null;
+            }
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            //use default loading view when data is being loaded via the remote views factory.
+            return null;
+        }
+
+
+        @Override
+        public int getViewTypeCount() {
+            //return different types of views to use in collection
+            return 1; //using only one layout.
+        }
+
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+}
